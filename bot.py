218282745c28f830c8eb9dd6ffbfa8de6497f88d
techflashYT <officialTechflashYT@gmail.com>
2024-07-26 13:14:14 -0700

    add message splitting and ansi support

--- bot.py
+++ bot.py
@@ -82,11 +82,19 @@ async def on_message(message):
 
                 shRe = subprocess.run("su -c '"+shSc+"' discord", shell=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
-                await myMsg.edit(content="```\n"+shRe.stdout+"\n```\n")
+                if len(shRe.stdout) > 3970:
+                    await myMsg.edit(content="**Warning**: Command output too much text, truncating\n```ansi\n"+shRe.stdout[0:1900]+"\n```\n")
+                    await myMsg.reply("```ansi\n"+shRe.stdout[1901:3800]+"\n```\n")
+                elif len(shRe.stdout) > 1970:
+                    await myMsg.edit(content="```ansi\n"+shRe.stdout[0:1900]+"\n```\n")
+                    await myMsg.reply("```ansi\n"+shRe.stdout[1901:-1]+"\n```\n")
+                else:
+                    await myMsg.edit(content="```ansi\n"+shRe.stdout+"\n```\n")
+
             except Exception as e:
                 content = "```\n"+f"We fucked up: {e}"+"\n```"
                 if myMsg:
-                    myMsg.edit(content=content)
+                    await myMsg.edit(content=content)
                 else:
                     await message.channel.send(content)
 # Get token
